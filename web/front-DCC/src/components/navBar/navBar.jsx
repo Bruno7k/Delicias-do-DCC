dd6e979932c52a671e6d674175b8fda663517951
Bruno Henrique <brunobhhf@gmail.com>
2025-01-26 23:53:26 -0300

    adicionando alguns detalhes

--- web/front-DCC/src/components/navBar/navBar.jsx
+++ web/front-DCC/src/components/navBar/navBar.jsx
@@ -4,12 +4,20 @@ import SearchIcon from "@mui/icons-material/Search";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import Logo from "../../assets/logoDCC.png";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 
 function NavBar() {
   const [searchVisible, setSearchVisible] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
   const [menuClosing, setMenuClosing] = useState(false);
   const searchRef = useRef(null);
+
+  const navigate = useNavigate();  // Cria a instância do navigate
+  const location = useLocation();  // Obtém a localização atual
+
+  const handleLogoClick = () => {
+    navigate("/home");  // Redireciona para a página '/home' quando o logo for clicado
+  };
 
   const toggleSearch = () => {
     setSearchVisible(!searchVisible);
@@ -43,45 +51,38 @@ function NavBar() {
   return (
     <div className="Componente-navBar">
       <div className="imagem-Nav">
-        <img src={Logo} className="logo" alt="Logo" />
+        <img src={Logo} className="logo" alt="Logo" onClick={handleLogoClick} />
       </div>
 
       <div
-        className={`hamburguer ${menuOpen ? "ativo" : ""} ${
-          menuClosing ? "fechar" : ""
-        }`}
+        className={`hamburguer ${menuOpen ? "ativo" : ""} ${menuClosing ? "fechar" : ""}`}
         onClick={toggleMenu}
       >
-        {menuOpen ? (
-          <CloseIcon className="icone-fechar" />
-        ) : (
-          <MenuIcon className="icone-hamburguer" />
-        )}
+        {menuOpen ? <CloseIcon className="icone-fechar" /> : <MenuIcon className="icone-hamburguer" />}
       </div>
 
-      <div
-        className={`texto-Nav ${menuOpen ? "visivel" : ""} ${
-          menuClosing ? "fechar" : ""
-        }`}
-      >
-        <a
-          href="/home"
-          className={window.location.pathname === "/home" ? "ativo" : ""}
+      <div className={`texto-Nav ${menuOpen ? "visivel" : ""} ${menuClosing ? "fechar" : ""}`}>
+        {/* HOME */}
+        <Link
+          to="/home"
+          className={location.pathname === "/home" ? "ativo" : ""}
         >
           HOME
-        </a>
-        <a
-          href="/receitas"
-          className={window.location.pathname === "/receitas" ? "ativo" : ""}
+        </Link>
+        {/* RECEITAS (Considerando o caso de rota dinâmica) */}
+        <Link
+          to="/receitas"
+          className={location.pathname.startsWith("/receitas") ? "ativo" : ""}
         >
           RECEITAS
-        </a>
-        <a
-          href="/favoritos"
-          className={window.location.pathname === "/favoritos" ? "ativo" : ""}
+        </Link>
+        {/* MEUS FAVORITOS */}
+        <Link
+          to="/favoritos"
+          className={location.pathname === "/favoritos" ? "ativo" : ""}
         >
           MEUS FAVORITOS
-        </a>
+        </Link>
       </div>
 
       <div
